65c3c64b145293ce959449bad3267fea000ba16e
HeitorDalla <heitorvillavilla@gmail.com>
2025-03-16 15:52:29 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-"use strict"
+"use strict";
+
+const containerCadastro = document.querySelector("#cadastro");
+
+// Função para criar elementos
+function newElement () {
+    const novoElemento = document.createElement("input");
+    novoElemento.setAttribute("class", "inputs");
+    containerCadastro.appendChild(novoElemento);
+    return novoElemento;
+};
+
+document.querySelector("#emailUsuari").style.displa = 'none';
+
+// Funcionalidades botao entrar
+const buttonEntrar = document.querySelector("#buttonEntrar");
+buttonEntrar.addEventListener("click", (event) => {
+    const name = document.querySelector("#nomeUsuario");
+    const nameValue = name.value;
+});
+
+// Funcionalidades botao cadastrar
+const buttonCadastrar = document.querySelector("#buttonCadastrar");
+buttonCadastrar.addEventListener("click", (event) => {
+    newElement();
+});
 
