52d21a660da7f26d91da7618cfa0494253deba33
HeitorDalla <heitorvillavilla@gmail.com>
2025-03-22 17:06:11 -0300

    Feito toda a parte de validacao inicial

--- script.js
+++ script.js
@@ -1,39 +1,101 @@
 "use strict"; // Ativa o modo estrito do Javascript
 
-const inputs = {}; // Objeto que contem todos os inputs (chave: id; valor: valor do campo)
-
-const containerCadastro = document.querySelector("#cadastro");
+const form = document.querySelector("#form");
 const containerInputs = document.querySelector("#inputs");
-
-// Função para criar elementos
-function newElement () {
-    const novoElemento = document.createElement("input");
-    novoElemento.setAttribute("class", "inputs");
-    containerCadastro.appendChild(novoElemento);
-    return novoElemento;
-};
-
-// Funcao para atualizar os inputs dentro do objeto global
-function atualizarInputs () {
-    const elementosInput = document.querySelectorAll(".inputs");
-    elementosInput.forEach((input) => {
-        inputs[input.id] = input.value;
-    })
-};
+const boxInput = [...document.querySelectorAll(".boxInput")];
+const inputNomeUsuario = document.querySelector("#nomeUsuario");
+const inputSenhaUsuario = document.querySelector("#senhaUsuario");
+const inputEmailUsuario = document.querySelector("#emailUsuario");
 
 // Oculta o campo de email inicialmente
-document.querySelector("#emailUsuario").style.display = 'none';
+inputEmailUsuario.style.display = 'none';
 
-// Funcionalidades botao entrar
-const buttonEntrar = document.querySelector("#buttonEntrar");
-buttonEntrar.addEventListener("click", (event) => {
-    const name = document.querySelector("#nomeUsuario");
-    const nameValue = name.value;
+// Função para criar elementos de erro
+function newElement (containerPai) {
+    let erroSpan = containerPai.querySelector(".error");
+
+    if (!erroSpan) { // Se ele nao existir, vai criar um elemento span
+        erroSpan = document.createElement("span");
+        erroSpan.setAttribute("class", "error");
+        erroSpan.setAttribute("display", "block");
+        erroSpan.innerHTML = '';
+        containerPai.appendChild(erroSpan);
+    }
+
+    return erroSpan;
+
+};
+
+// Funcao para a verificacao de valor vazia
+function isEmpty (value) {
+    if (value === '') {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+// Funcao para validar os campos
+function validacaoDados (value) {
+    const validator = {
+        eValid: true,
+        mensagemErro: null
+    }
+
+    if (isEmpty(value)) {
+        validator.eValid = false;
+        validator.mensagemErro = 'O campo esta vazio!';
+        return validator;
+    }
+
+    const tamanhoMinimo = 8;
+    if (value.length < tamanhoMinimo) {
+        validator.eValid = false;
+        validator.mensagemErro = `O campo possui menos de ${tamanhoMinimo} caracteres!`;
+        return validator;
+    }
+
+    return validator;
+
+};
+
+// Para fazer as validacoes de inputs
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let formIsValid = true;
+    const iconeErro = '<i class="fa-solid fa-circle-exclamation"></i>';
+
+    // Nome
+    const nomeValue = inputNomeUsuario.value;
+    const containerPaiNome = inputNomeUsuario.closest(".boxInput");
+    let erroSpanName =  newElement(containerPaiNome);
+
+    const validacaoNome = validacaoDados(nomeValue);
+
+    if (!validacaoNome.eValid) {
+        formIsValid = false;
+        erroSpanName.innerHTML = `${iconeErro} ${validacaoNome.mensagemErro}`;
+    } else {
+        erroSpanName.innerHTML = '';
+    }
+
+    // Senha
+    const senhaValue = inputSenhaUsuario.value;
+    const containerPaiSenha = inputSenhaUsuario.closest(".boxInput");
+    let  erroSpanSenha = newElement(containerPaiSenha);
+
+    const validacaoSenha = validacaoDados(senhaValue);
+
+    if (!validacaoSenha.eValid) {
+        formIsValid = false;
+        erroSpanSenha.innerHTML = `${iconeErro} ${validacaoSenha.mensagemErro}`;
+    } else {
+        erroSpanSenha.innerHTML = '';
+    }
+
+    if (formIsValid) {
+        form.submit();
+    }
+
 });
-
-// Funcionalidades botao cadastrar
-const buttonCadastrar = document.querySelector("#buttonCadastrar");
-buttonCadastrar.addEventListener("click", (event) => { 
-    newElement();
-});
-
