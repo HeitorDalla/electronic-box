0f74ed21260b46cba1073bb592caebd0340e1b14
HeitorDalla <88400131+HeitorDalla@users.noreply.github.com>
2025-03-23 22:40:47 -0300

    Feito a parte de depositar um valor financeiro

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ const buttonVoltar = document.querySelector("#buttonVoltar");
 
 // Container registrado
 const containerRegistrado = document.querySelector("#registrado");
+const nome = document.querySelector("#nome");
+const conta = document.querySelector("#conta")
+const saldo = document.querySelector("#saldo");
 const buttonsRegistrados = [...document.querySelectorAll("#buttonsRegistros .buttonsRegistrados")];
 
 // Variaveis para manipular a quantidade de tempo que o usuario fica nas telas do financeiro
@@ -22,7 +25,7 @@ let intervaloTempo; // Armazenara o tempo
 let tempoIniciado = false; // Ve se o tempo iniciou
 
 // Funcao para a verificacao de valor vazia
-function isEmpty (value) {
+function isEmpty(value) {
     if (value === '') {
         return true;
     } else {
@@ -31,9 +34,9 @@ function isEmpty (value) {
 };
 
 // Função para criar elementos de erro
-function newElement (containerPai) {
+function newElement(containerPai) {
     let erroSpan = containerPai.querySelector(".error");
-    
+
     if (!erroSpan) { // Se ele nao existir, vai criar um elemento span
         erroSpan = document.createElement("span");
         erroSpan.setAttribute("class", "error");
@@ -47,7 +50,7 @@ function newElement (containerPai) {
 };
 
 // Funcao para limpar campos
-function limparCampos () {
+function limparCampos() {
 
     // Limpar todos os campos de erro
     document.querySelectorAll(".error").forEach((spanError) => {
@@ -58,10 +61,12 @@ function limparCampos () {
     inputSenhaUsuario.value = '';
     inputEmailUsuario.value = '';
 
+    document.querySelector("#depositar").value = '';
+
 };
 
 // Funcao para gerar um codigo de conta novo a cada cliente
-function gerarCodigo () {
+function gerarCodigo() {
 
     const primeiroDigito = 4;
     const outrosDigitos = Math.floor(Math.random() * 1000);
@@ -71,7 +76,7 @@ function gerarCodigo () {
 };
 
 // Funcao para validar os campos
-function validacaoDados (value) {
+function validacaoDados(value) {
     const validator = {
         eValid: true,
         mensagemErro: null
@@ -83,7 +88,7 @@ function validacaoDados (value) {
         return validator;
     }
 
-    const tamanhoMinimo = 8;
+    const tamanhoMinimo = 6;
     if (value.length < tamanhoMinimo) {
         validator.eValid = false;
         validator.mensagemErro = `O campo possui menos de ${tamanhoMinimo} caracteres!`;
@@ -110,7 +115,7 @@ function validacaoDados (value) {
 };
 
 // Funcao
-function validacao () {
+function validacao() {
 
     let isValid = true;
     const iconeErro = '<i class="fa-solid fa-circle-exclamation"></i>';
@@ -118,7 +123,7 @@ function validacao () {
     // Nome
     const nomeValue = inputNomeUsuario.value;
     const containerPaiNome = inputNomeUsuario.closest(".boxInput");
-    let erroSpanName =  newElement(containerPaiNome);
+    let erroSpanName = newElement(containerPaiNome);
 
     const validacaoNome = validacaoDados(nomeValue);
 
@@ -132,7 +137,7 @@ function validacao () {
     // Senha
     const senhaValue = inputSenhaUsuario.value;
     const containerPaiSenha = inputSenhaUsuario.closest(".boxInput");
-    let  erroSpanSenha = newElement(containerPaiSenha);
+    let erroSpanSenha = newElement(containerPaiSenha);
 
     const validacaoSenha = validacaoDados(senhaValue);
 
@@ -142,15 +147,15 @@ function validacao () {
     } else {
         erroSpanSenha.innerHTML = '';
     }
-    
+
     // Email
     if (!(inputEmailUsuario.style.display === 'none')) {
         const emailValue = inputEmailUsuario.value;
         const containerPaiEmail = inputEmailUsuario.closest(".boxInput");
         let erroSpanEmail = newElement(containerPaiEmail);
-    
+
         const validacaoEmail = validacaoDados(emailValue);
-    
+
         if (!validacaoEmail.eValid) {
             isValid = false;
             erroSpanEmail.innerHTML = `${iconeErro} ${validacaoEmail.mensagemErro}`
@@ -163,6 +168,10 @@ function validacao () {
         limparCampos();
         containerCadastro.style.display = 'none';
         containerRegistrado.style.display = 'block';
+
+        // Colocar informacoes do usuario
+        nome.textContent = nomeValue;
+        conta.textContent = gerarCodigo();
     }
 };
 
@@ -189,6 +198,7 @@ buttonCadastrar.addEventListener("click", (event) => {
 // Funcionalidades do botao 'voltar ao entrar'
 buttonVoltar.addEventListener("click", (event) => {
     event.preventDefault();
+
     limparCampos();
 
     inputEmailUsuario.style.display = 'none';
@@ -206,11 +216,32 @@ buttonVoltar.addEventListener("click", (event) => {
 
 
 // Funcionalidades para o botao de depositar
-const buttonDepositar = document.querySelector("#buttonDepositar");
+const buttonDepositar = document.querySelector(".buttonDepositar");
+buttonDepositar.addEventListener("click", (event) => {
+    event.preventDefault();
 
-// Função para iniciar o tempo
-function contagemTempo () {
-    const delay = setTimeout (() => {
+    document.querySelector("#deposito").style.display = 'block';
+    
+    document.querySelector("#deposito .buttonsDepositar").addEventListener("click", (event) => {
+        event.preventDefault();
 
-    }, 500);
-};
+        const depositoInput = document.querySelector("#depositar").value;
+
+        if (isNaN(depositoInput) || depositoInput <= 0) {
+            alert("Digite um valor valido!");
+            return;
+        }
+
+        let saldoAtual = parseFloat(saldo.textContent.replace("R$ ", ""));
+        let saldoNovo = saldoAtual + parseFloat(depositoInput);
+
+        saldo.textContent = `R$: ${saldoNovo.toFixed(2)}`;
+
+        limparCampos();
+
+        alert(`Foi depositado um valor de R$ ${depositoInput},00`);
+
+        document.querySelector("#deposito").style.display = 'none';
+    })
+
+});
